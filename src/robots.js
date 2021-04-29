38e802715511db233c2905551bab689c862e45f9
Helen <helenonewhouse@gmail.com>
2021-04-29 12:20:17 -0400

    adding changes

---
+++ src/robots.js
@@ -0,0 +1,50 @@
+export const robots = [
+    {
+        id: 1,
+        name: `Leanne Graham`,
+        username: `Bret`,
+        email: `Sincere@april.biz`
+    },
+    {
+        id: 2,
+        name: `Ervin Howell`,
+        username: `Antonette`,
+        email: `Shanna@melissa.tv`
+    },
+    {
+        id: 3,
+        name: `Clementine Bauch`,
+        username: `Samantha`,
+        email: `Nathan@yesenia.net`
+    },
+    {
+        id: 4,
+        name: `Patricia Lebsack`,
+        username: `Marianne`,
+        email: `Julianne.OConner@kory.org`
+    },
+    {
+        id: 5,
+        name: `Cybele Nuevacasa`,
+        username: `LeeLee`,
+        email: `catzrule@bengals.com`
+    },
+    {
+        id: 6,
+        name: `Bobby Brandham`,
+        username: `Jason`,
+        email: `bbthrei@kien.org`
+    },
+    {
+        id: 7,
+        name: `Natasha Nuevacasa`,
+        username: `Tishafish`,
+        email: `bluesarebetter@russianblue.org`
+    },
+    {
+        id: 8,
+        name: `Joe Massman`,
+        username: `GoPats`,
+        email: `massbutts@mass.gov`
+    }
+]
